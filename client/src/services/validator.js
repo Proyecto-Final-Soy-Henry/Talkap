5e0a18f232949ec1b07b336951a66fef32bae02f
Wild-Design <brenneke_ruger@hotmail.com>
2023-01-18 20:47:19 -0300

    Actualizacion para deploy del Front

--- client/src/services/validator.js
+++ client/src/services/validator.js
@@ -1,12 +1,10 @@
-export  function userValidator (user){
-
-    fetch('http://localhost:3001',{
-        method:'POST',
-        body:JSON.stringify(user),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-       })
-
-
+export function userValidator(user) {
+  // fetch('http://localhost:3001',{
+  fetch("https://talkap-production.up.railway.app/", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
