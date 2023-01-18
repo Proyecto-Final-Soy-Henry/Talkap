c5c1722a58de0b3e2e0b4a6521e930f207770539
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-01-18 16:07:22 -0300

    Se Crea el componente UserList, donde se va a visualizar los usuarios

--- client/src/component/UserList/UserList.jsx
+++ client/src/component/UserList/UserList.jsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux"
+import {getUserList} from '../../services/getUserList.js'
 export default function UserList(){
+    useEffect(()=>{
+       getUserList() 
+    },[])
     const {list} =  useSelector(state=>state.users);
     return(<div className='user-list'>
 
