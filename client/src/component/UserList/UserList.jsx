d048b342fc6892ea1bbfccf235c838b98acf1069
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-01-18 16:54:05 -0300

    Se configura UserList, para obtener los usuarios del estado global

--- client/src/component/UserList/UserList.jsx
+++ client/src/component/UserList/UserList.jsx
@@ -1,12 +1,24 @@
+import './UserList.css'
 import { useEffect } from "react";
-import { useSelector } from "react-redux"
+import {useDispatch, useSelector } from "react-redux"
 import {getUserList} from '../../services/getUserList.js'
-export default function UserList(){
-    useEffect(()=>{
-       getUserList() 
-    },[])
-    const {list} =  useSelector(state=>state.users);
-    return(<div className='user-list'>
 
+export default function UserList(){
+    const dispatch = useDispatch();
+    
+    useEffect(()=>{
+       getUserList(dispatch) 
+    },[dispatch])
+   
+     const {list} =  useSelector(state=>state.users)
+    return(<div className='user-list'>
+        <h1>Lista de Usuarios TALKAP</h1>
+        {list?.map((user,index)=>{
+            return <div key={index}>
+                <h3>{user.name}</h3>
+                <img src={user.picture} alt={user.name}/>
+                <br/>
+            </div>
+        })}
     </div>)
 }
