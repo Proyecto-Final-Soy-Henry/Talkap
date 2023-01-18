7728f9371d87067ffa422df28c3385336ce71f5e
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-01-18 16:55:10 -0300

    Se configura el servicio gerUserList

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const handleUser =  require('./handlers/handleUser.js');
+const userListHandler = require('./handlers/userListHandler.js');
 const router = Router();
 
 router.post('/',handleUser);
