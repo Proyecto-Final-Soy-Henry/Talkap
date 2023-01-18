b6eca71d400fb310897e6d5135f213183755c1e3
Wild-Design <brenneke_ruger@hotmail.com>
2023-01-18 19:56:05 -0300

    Actualizo ruta DB para deploy

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const server = require('./src/app.js')
-const {conn} = require('./src/db.js');
-require('dotenv').config();
-const {PORT} =  process.env;
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
 
 
-conn.sync({force:true}).then(()=>{
-    server.listen(PORT,()=>{
-        console.log('listening at PORT '+PORT)
-    });
+
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log("listening at PORT " + PORT);
+  });
 });
- 
