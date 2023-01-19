27607fb01d21fde3659dadd0a7dd3cb3eb8cf374
Renzo Doratto <renzodoratto1@hotmail.com>
2023-01-19 15:03:05 -0300

    Arreglo Login Mail

--- server/src/routes/handlers/handleUser.js
+++ server/src/routes/handlers/handleUser.js
@@ -10,13 +10,28 @@ where:{
 });
 
 if(user.length===0){
+    
+    if(name.includes("@")){
+        let newName =[]
+        for(let i = 0; name[i] !== "@"; i++){
+            newName.push(name[i])   
+        }   
+        let name1 = newName.join("")
+        const userCreado = await User.create({
+            name: name1,
+            email,
+            picture,
+        })
+        
+    } else {
     const userCreado = await User.create({
         name,
         email,
         picture,
     }).catch((error)=>{console.log('')})
-    console.log('usuario creado')
-    res.status(200).send(userCreado);
+    console.log('usuario creado',name)
+    res.status(200).send(userCreado);}
+      
 }else{
     console.log('usuario ya estaba creado');
     res.status(200).send(user);
