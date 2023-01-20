a24af5195214b991f5a4e9215f301f6617413f5f
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-01-20 06:34:41 -0300

    se retoca ChatRender

--- client/src/component/ChatRender/ChatRender.jsx
+++ client/src/component/ChatRender/ChatRender.jsx
@@ -1,11 +1,12 @@
 import './ChatRender.css'
 export default function  ChatRender({menssages}){
+
     return (<div className="chat-render">
         {menssages?.map((msj,index)=>{
            
           return <div key={index}>
-                  <p>{msj.user}</p>
-                  <p>{msj.message}</p>
+                  <p>{msj.user} : {msj.message}</p>
+                  <br/>
           </div>
     
           
