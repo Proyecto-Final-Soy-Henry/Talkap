21dadabc61dd25f239384865a3bd98023f3e44c7
Wild-Design <brenneke_ruger@hotmail.com>
2023-01-20 19:02:46 -0300

    coso

--- client/src/component/Login/Login.jsx
+++ client/src/component/Login/Login.jsx
@@ -3,6 +3,9 @@ import LoginButton from "../LoginButton/LoginButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import Logo from "./LogoTransparent.png";
+import { motion } from "framer-motion";
+
 export default function Login() {
   const { isAuthenticated } = useAuth0();
   const navigate = useNavigate();
@@ -21,6 +24,22 @@ export default function Login() {
           <LoginButton />
         </>
       )}
+      <motion.div
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 180, 180, 0],
+          borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          repeatDelay: 1,
+        }}
+        className={motion.img}
+      >
+        <img src={Logo} alt="Logo" />
+      </motion.div>
     </div>
   );
 }
