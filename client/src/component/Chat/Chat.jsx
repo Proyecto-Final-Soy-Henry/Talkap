ed4dd6eb5cfbf4e2998b49735bfc61c7f9659cf8
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-01-21 09:42:19 -0300

    reload

--- client/src/component/Chat/Chat.jsx
+++ client/src/component/Chat/Chat.jsx
@@ -3,12 +3,11 @@ import ChatRender from '../ChatRender/ChatRender.jsx';
 import ChatInput from '../ChatInput/ChatInput.jsx';
 import {sendMessage} from  '../../services/sockets.js';
 import { useSelector } from 'react-redux';
-import { useAuth0 } from '@auth0/auth0-react';
 export default function Chat(){
  const {list} = useSelector(state=>state.chat);
- const { user } = useAuth0();
+ const {my} = useSelector(state=>state.users);
  const buttonHandler = (msj)=>{
-    sendMessage('chat',{user:user.name,message:msj})
+    sendMessage('chat',{user:my.name,message:msj})
    }
    return (<div className='chat'>
 
