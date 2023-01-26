a1a169673b410b7946fac974f7b942553dc5b326
Renzo Doratto <renzodoratto1@hotmail.com>
2023-01-25 22:12:50 -0300

    chat visual

--- client/src/component/UserCard/UserCard.jsx
+++ client/src/component/UserCard/UserCard.jsx
@@ -1,8 +1,16 @@
 import './UserCard.css'
 export default function UserCard({user,handle}){
-    return (<div key={user.name} className="user-card">
-        <img onClick={()=>{handle(user)}} className='img' src={user.picture} alt={user.name}/>
-        <p>{user.connected?'✅ ':'⛔ '}{user.name}</p>
+    let name1 = user.name
+    if(user.name.includes("@")){
+        let newName =[]
+        for(let i = 0; user.name[i] !== "@"; i++){
+            newName.push(user.name[i])   
+        }   
+        name1 = newName.join("")}
+  
+    return (<div key={name1} className="user-card">
+        <img onClick={()=>{handle(user)}} className='img' src={user.picture} alt={name1}/>
+        <p>{user.connected?'✅ ':'⛔ '}{name1}</p>
         
     </div>);
 }
