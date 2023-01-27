3fe35cf4f28e16a12c7ba4538679eb8d8abfc506
Renzo Doratto <renzodoratto1@hotmail.com>
2023-01-26 23:23:33 -0300

    no aparece tu usuario en la lista de usuarios, + cambios en el css de la userlist

--- client/src/component/UserCard/UserCard.jsx
+++ client/src/component/UserCard/UserCard.jsx
@@ -1,16 +1,28 @@
+import { useSelector } from 'react-redux'
 import './UserCard.css'
 export default function UserCard({user,handle}){
+
+    const {my} =  useSelector(state=>state.users)
+
     let name1 = user.name
+    let conne = "disconnec"
+    let email = user.email == my.email
     if(user.name.includes("@")){
         let newName =[]
         for(let i = 0; user.name[i] !== "@"; i++){
             newName.push(user.name[i])   
         }   
         name1 = newName.join("")}
-  
-    return (<div key={name1} className="user-card">
-        <img onClick={()=>{handle(user)}} className='img' src={user.picture} alt={name1}/>
-        <p>{user.connected?'✅ ':'⛔ '}{name1}</p>
+    if(user.connected){
+        conne= "connec"
+    }
+   
+    return (
         
-    </div>);
+        <div key={name1} className="user-card">
+            {!email&& <div> <img onClick={()=>{handle(user)}} className={conne} src={user.picture} alt={name1}/>
+        <p>{name1}</p></div>}
+       
+        </div>
+        );
 }
