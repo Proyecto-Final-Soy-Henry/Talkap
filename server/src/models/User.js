568e30ea4ffc185183345039bbc699241bb6c3e8
Wild-Design <brenneke_ruger@hotmail.com>
2023-01-31 19:29:03 -0300

    Inputs del chat arreglados

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,62 +1,51 @@
-const {DataTypes} = require ('sequelize'); 
-module.exports = (sequelize)=>{
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define("user", {
+    // id:{
+    //     type:DataTypes.UUID,
+    //     defaultValue: DataTypes.UUIDV4,
 
-sequelize.define('user',{
-// id:{
-//     type:DataTypes.UUID,
-//     defaultValue: DataTypes.UUIDV4,
- 
-// },
-nick:{
-    type:DataTypes.STRING,
-},
-name:{
-    type:DataTypes.STRING,
-    allowNull:false,
-},
-apellido:{
-    type:DataTypes.STRING,
+    // },
+    nick: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    picture: {
+      type: DataTypes.STRING,
+    },
+    connected: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    bio: {
+      type: DataTypes.STRING,
+    },
+    type: {
+      type: DataTypes.STRING,
+    },
+    users: {
+      type: DataTypes.JSON,
+    },
+    admin: {
+      type: DataTypes.JSON,
+    },
+    groupName: {
+      type: DataTypes.STRING,
+    },
 
-},
-email:{
-    type:DataTypes.STRING,
-    allowNull:false,
-    primaryKey:true,
-},
-picture:{
-    type:DataTypes.STRING,
-    
-},
-connected:{
-    type:DataTypes.BOOLEAN,
-    defaultValue:false,
-},
-bio:{
-    type:DataTypes.STRING,
-},
-type:{
-    type:DataTypes.STRING,
-},
-users:{
-    type:DataTypes.JSON,
-},
-admin:{
-    type:DataTypes.JSON
-},
-groupName :{
-    type:DataTypes.STRING,
-},
-
-Favourite:{
-    type:DataTypes.JSON,
-},
-
-
-
-//comentario
-//comentario dos
-//comentario tres
-
-
-});
+    Favourite: {
+      type: DataTypes.JSON,
+    },
+  });
 };
