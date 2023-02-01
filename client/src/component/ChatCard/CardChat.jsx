a555d3af21aaf702f3f3e331e3922cd9d9d7db3e
Renzo Doratto <renzodoratto1@hotmail.com>
2023-02-01 14:52:59 -0300

    cambios en CHat grupal, userlist y ChatCard

--- client/src/component/ChatCard/CardChat.jsx
+++ client/src/component/ChatCard/CardChat.jsx
@@ -1,5 +1,13 @@
 import style from './ChatCard.module.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import { setAddressee } from '../../store/slices/users';
+import { useDispatch } from 'react-redux';
+
 export default function ChatCard({picture,email}){
+    const dispatch = useDispatch();
+    const handle = (user) => {
+        dispatch(setAddressee(user));
+      };
 
     //////////////////////Trasforma Emaill en un Name//////////////////////
     let name1 = ""
@@ -10,9 +18,14 @@ export default function ChatCard({picture,email}){
     name1 = newName.join("")
     //////////////////////////////////////////////////////////////////////
     return(<div className={style.chatcard}>
-        
+       
         <img alt={email} src={picture}/>
         <p>{name1.toUpperCase()}</p>
+        <div className={style.div}>
+        <AiOutlineHome onClick={()=>{handle()}} size="22px" ></AiOutlineHome>
+        </div>
+        
+        
         
     </div>)
 }
