85543d00949b699603393ef2a3289b5b4440b283
GuidoGattini <gttnguido@gmail.com>
2023-02-01 17:10:18 -0300

    cambios de css en chatChard

--- client/src/component/ChatCard/CardChat.jsx
+++ client/src/component/ChatCard/CardChat.jsx
@@ -1,31 +1,31 @@
 import style from './ChatCard.module.css'
-import {AiOutlineHome} from 'react-icons/ai'
+import { AiOutlineHome } from 'react-icons/ai'
 import { setAddressee } from '../../store/slices/users';
 import { useDispatch } from 'react-redux';
 
-export default function ChatCard({picture,email}){
+export default function ChatCard({ picture, email }) {
     const dispatch = useDispatch();
     const handle = (user) => {
         dispatch(setAddressee(user));
-      };
+    };
 
     //////////////////////Trasforma Emaill en un Name//////////////////////
     let name1 = ""
-    let newName =[]
-    for(let i = 0; email[i] !== "@"; i++){
-        newName.push(email[i])   
-    }   
+    let newName = []
+    for (let i = 0; email[i] !== "@"; i++) {
+        newName.push(email[i])
+    }
     name1 = newName.join("")
     //////////////////////////////////////////////////////////////////////
-    return(<div className={style.chatcard}>
-       
-        <img alt={email} src={picture}/>
-        <p>{name1.toUpperCase()}</p>
-        <div className={style.div}>
-        <AiOutlineHome onClick={()=>{handle()}} size="22px" ></AiOutlineHome>
+    return (
+        <div className={style.chatcard}>
+            <div className={style.container}>
+            <img alt={email} src={picture} />
+            <p>{name1.toUpperCase()}</p>
+            <div className={style.home}>
+                <AiOutlineHome onClick={() => { handle() }} size="40px" ></AiOutlineHome>
+            </div>
+            </div>
         </div>
-        
-        
-        
-    </div>)
+    )
 }
