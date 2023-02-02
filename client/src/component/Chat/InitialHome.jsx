c2c7d2e5a1642f1f7a2320bd4e5dfa4d84e309ba
Renzo Doratto <renzodoratto1@hotmail.com>
2023-02-02 20:43:23 -0300

    fix

---
+++ client/src/component/Chat/InitialHome.jsx
@@ -0,0 +1,26 @@
+import style from "./InitialHome.module.css";
+import donationImage from "../../assets/donationImage.png"
+import friendsImage from "../../assets/friendsImage.png"
+
+export default function InitialHome() {
+    return (
+        <div className={style.container}>
+            <div className={style.data}>
+                <div className={style.divTalkap}>
+                    <div className={style.talkapText}>
+                    <h1 className={style.talkap}>TALKAP</h1>
+                    <h3 className={style.infoTalkap}>Invita y agrega amigos de cualquier parte del mundo</h3>
+                    </div>
+                    <img src={friendsImage} alt="" className={style.img1} />
+                </div>
+                <div className={style.divDonacion}>
+                    <div className={style.donacionLeft}>
+                    <h2 className={style.title}>DONACIÓN</h2>
+                    <button className={style.donationButton}>Hacé tu donación</button>
+                    </div>
+                    <img src={donationImage} alt="" className={style.img2} />
+                </div>
+            </div>
+        </div>
+    )
+}
