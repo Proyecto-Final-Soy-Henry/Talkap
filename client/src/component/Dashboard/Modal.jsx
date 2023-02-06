35b63aeac166f3c9304a4b8493327465fabfb47f
Wild-Design <brenneke_ruger@hotmail.com>
2023-02-05 22:31:11 -0300

    Arreglo scroll y responsive al coso de banear los usuarios

--- client/src/component/Dashboard/Modal.jsx
+++ client/src/component/Dashboard/Modal.jsx
@@ -1,21 +1,34 @@
-import style from './Modal.module.css'
-import { useSelector } from "react-redux"
-import Card from './Card'
-import { sendMessage } from '../../services/sockets'
-export default function Modal ({setModal}){
-const {list} =  useSelector(state=>state.users) 
+import style from "./Modal.module.css";
+import { useSelector } from "react-redux";
+import Card from "./Card";
+import { sendMessage } from "../../services/sockets";
+export default function Modal({ setModal }) {
+  const { list } = useSelector((state) => state.users);
 
-return (<div className={style.modal}>
-
-<button className={style.buttonExit} onClick={()=>{setModal(false)}}>Cerrar</button>
-
-{list.filter(e=>e.type==='user').map(e=>{
-   return <Card user={e} 
-   handle={()=>{sendMessage('blacklist',e.email)}
-}/>
-    
-})}
-    
-
-</div>)
+  return (
+    <>
+      <button
+        className={style.buttonExit}
+        onClick={() => {
+          setModal(false);
+        }}
+      >
+        Cerrar
+      </button>
+      <div className={style.modal}>
+        {list
+          .filter((e) => e.type === "user")
+          .map((e) => {
+            return (
+              <Card
+                user={e}
+                handle={() => {
+                  sendMessage("blacklist", e.email);
+                }}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
 }
