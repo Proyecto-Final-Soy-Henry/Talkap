e3f649e463b9198d3bd44559e1947ba967716906
GuidoGattini <gttnguido@gmail.com>
2023-02-07 15:16:07 -0300

    casi listo, faltan detalles

--- client/src/component/RightHome/RightHome.jsx
+++ client/src/component/RightHome/RightHome.jsx
@@ -11,6 +11,7 @@ import UserList from "../UserList/UserList.jsx";
 import FriendsList from "../FriendsList/FriendsList.jsx"
 
 import style from "./RightHome.module.css";
+import ButtonRight from "./ButtonRight.jsx";
 
 const RightHome = () => {
   const colors = useColorModeValue(["#2322239c", "#2322239c"]);
@@ -18,34 +19,52 @@ const RightHome = () => {
   const [tabIndex, setTabIndex] = useState(0);
   const bg = colors[tabIndex];
 
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    setClicked(!clicked)
+  }
+
   return (
-    <Tabs className={style.container} isFitted onChange={(index) => setTabIndex(index)} bg={bg} color="black">
-      <TabList>
-        <Tab
+    <div className={style.container}>
+      <div className={style.buttonRight}>
+        <ButtonRight clicked={clicked} handleClick={handleClick} />
+      </div>
+      <Tabs
+        isFitted
+        onChange={(index) => setTabIndex(index)}
+        bg={bg}
+        color="black"
+      >
+        <div className={`${style.rightHead} ${clicked ? style.active : ""}`}>
+          <TabList>
+            <Tab
 
-          color="white"
-          _focus={{ color: "#fe4e5b" }}
-          fontWeight="semibold"
-        >
-          Usuarios
-        </Tab>
+              color="white"
+              _focus={{ color: "#fe4e5b" }}
+              fontWeight="semibold"
+            >
+              Usuarios
+            </Tab>
 
-        <Tab
-          color="white"
-          _focus={{ color: "#fe4e5b" }}
-          fontWeight="semibold"
-        >
-          Amigos
-        </Tab>
-      </TabList>
-      <TabPanels>
-        {/* <TabPanel overflow="hidden" overflowY="scroll"> */}
-       <TabPanel>
-          {<UserList />}
-        </TabPanel>
-        <TabPanel>{<FriendsList/>}</TabPanel>
-      </TabPanels>
-    </Tabs>
+            <Tab
+              color="white"
+              _focus={{ color: "#fe4e5b" }}
+              fontWeight="semibold"
+            >
+              Amigos
+            </Tab>
+          </TabList>
+          <TabPanels>
+            {/* <TabPanel overflow="hidden" overflowY="scroll"> */}
+            <TabPanel>
+              {<UserList />}
+            </TabPanel>
+            <TabPanel>{<FriendsList />}</TabPanel>
+          </TabPanels>
+        </div>
+      </Tabs>
+    </div>
   );
 };
 
