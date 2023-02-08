ac7c094c235d207dddaff5b0fd6d565a94de7174
Wild-Design <brenneke_ruger@hotmail.com>
2023-02-07 21:36:14 -0300

    Agrego nueva imagen al mail de baneo

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,8 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 const handleOneUser = require("./handlers/handleCurrentUser.js");
-const coso = require("./handlers/sendMail");
-coso("brenneke.ruger@gmail.com", "ban");
+
 router.get(`/users/:email`, handleOneUser);
 
 module.exports = router;
